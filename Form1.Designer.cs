7c7a74db897f6322b84edad2bdc9cb4b8f9dc4b4
Галина Макенова <makenova11@mail.ru>
2021-12-11 20:14:57 +0700

    Добавлено разделение на сферы

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,6 +34,17 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // AnT
@@ -62,36 +73,124 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(38, 29);
+            this.button1.Location = new System.Drawing.Point(24, 326);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(155, 28);
+            this.button1.Size = new System.Drawing.Size(200, 28);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Обновить";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(38, 77);
+            this.button2.Location = new System.Drawing.Point(38, 96);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(155, 30);
+            this.button2.Size = new System.Drawing.Size(115, 30);
             this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
+            this.button2.Text = "Цвет";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(38, 131);
+            this.button3.Location = new System.Drawing.Point(38, 155);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(155, 24);
+            this.button3.Size = new System.Drawing.Size(155, 28);
             this.button3.TabIndex = 3;
-            this.button3.Text = "button3";
+            this.button3.Text = "Текстура";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "X",
+            "Y",
+            "Z"});
+            this.comboBox1.Location = new System.Drawing.Point(38, 45);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(155, 24);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Ось растяжения";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(159, 96);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(34, 30);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 21);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(56, 20);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Wire";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(24, 211);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 109);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Тип сферы";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(12, 74);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(73, 20);
+            this.radioButton3.TabIndex = 9;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Texture";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 48);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(59, 20);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Solid";
+            this.radioButton2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(858, 558);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -99,6 +198,9 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -111,6 +213,15 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -21,13 +21,357 @@ namespace Lb6Paint
         {
             InitializeComponent();
             AnT.InitializeContexts();
+            // устанавливаем ось X по умолчанию
+            comboBox1.SelectedIndex = 0;
         }
 
         bool showTexture = true; // Выводим сферу с текстурой
         bool useMaterial = true; // Используем материал и источники света
         bool showNormals = true; // Показываем нормали
+
+        // Смещение
+        private float x_tr = 0;
+        private float y_tr = 0;
+        private float z_tr = 0;
+        //Масштаб 
+        private float y_scale = 2;
+        private float x_scale = 2;
+        private float z_scale = 2;
+        //Цвет
+        private Color Color = Color.Blue;
+        // загружена ли текстура
+        private bool textureIsLoad = false;
+        // идентификатор текстуры
+        public int imageId = 0;
+        // текстурный объект
+        public uint mGlTextureObject = 0;
+
+
         private void Form1_Load(object sender, EventArgs e)
         {
+            Glut.glutInit();
+            Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH);
+            Gl.glClearColor(0, 255, 255, 1);
+            // установка порта вывода в соответствии с размерами элемента anT
+            Gl.glViewport(0, 0, AnT.Width, AnT.Height);
+            // настройка проекции
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            Gl.glLoadIdentity();
+            Glu.gluPerspective(45, (float)AnT.Width / (float)AnT.Height, 0.1, 200);
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+            Gl.glLoadIdentity();
+            // настройка параметров OpenGL для визуализации
+            Gl.glEnable(Gl.GL_DEPTH_TEST);
+            // начало визуализации (активируем таймер)
+            RenderTimer.Start();
+            // инициализация библиотеки openIL
+            Il.ilInit();
+            Il.ilEnable(Il.IL_ORIGIN_SET);
+
+            string path = "C:\\Users\\aken\\Desktop\\cosmos";
+
+        }
+
+        private void showSolid()
+        {
+            Glut.glutSolidSphere(0.75, 16, 16); // Сфера
+        }
+
+        /// <summary>
+            /// Отрисовка примитива
+            /// </summary>
+            private void Draw()
+        {
+            if (radioButton1.Checked)//wire
+            {
+                Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
+                Gl.glLoadIdentity();
+                Gl.glColor3f(Color.R, Color.G, Color.B);
+                // масштаб
+                Gl.glScalef(x_scale, y_scale, z_scale);
+                Gl.glPushMatrix();
+                Gl.glTranslated(0, 0, -6);
+                // перемещаем камеру для более хорошего обзора объекта
+                Gl.glTranslated(x_tr, y_tr, z_tr);
+                Gl.glRotated(angle, 1, 1, 0);
+                // рисуем сферу с помощью библиотеки FreeGLUT
+                Glut.glutWireSphere(0.7, 30, 30);
+                Gl.glPopMatrix();
+                Gl.glFlush();
+                AnT.Invalidate();
+            }
+            else if (radioButton2.Checked)//solid
+            {
+                Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
+                Gl.glLoadIdentity();
+                Gl.glColor3f(Color.R, Color.G, Color.B);
+                // масштаб
+                Gl.glScalef(x_scale, y_scale, z_scale);
+                Gl.glPushMatrix();
+                Gl.glTranslated(0, 0, -6);
+                // перемещаем камеру для более хорошего обзора объекта
+                Gl.glTranslated(x_tr, y_tr, z_tr);
+                Gl.glRotated(angle, 1, 1, 0);
+                // рисуем сферу с помощью библиотеки FreeGLUT
+                Glut.glutSolidSphere(0.7, 30, 30);
+
+                // Модель освещенности с одним источником цвета
+                float[] light_position = { 100, 0, 0, 0 }; // Координаты источника света
+                float[] lghtClr = { 1, 1, 1, 0 }; // Источник излучает белый цвет
+                float[] mtClr = { Color.R, Color.G, Color.B, 0 }; // Материал 
+                Gl.glPolygonMode(Gl.GL_FRONT, Gl.GL_FILL); // Заливка полигонов
+                Gl.glShadeModel(Gl.GL_SMOOTH); // Вывод с интерполяцией цветов
+                Gl.glEnable(Gl.GL_LIGHTING); // Будем рассчитывать освещенность
+                Gl.glLightfv(Gl.GL_LIGHT0, Gl.GL_POSITION, light_position);
+                Gl.glLightfv(Gl.GL_LIGHT0, Gl.GL_AMBIENT, lghtClr); // Рассеивание
+                Gl.glEnable(Gl.GL_LIGHT0); // Включаем в уравнение освещенности источник GL_LIGHT0
+                // Диффузионная компонента цвета материала
+                Gl.glMaterialfv(Gl.GL_FRONT, Gl.GL_DIFFUSE, mtClr);
+                // Выводим тонированный glut-примитив
+                showSolid();
+                Gl.glDisable(Gl.GL_LIGHTING); // Будем рассчитывать освещенность
+
+                Gl.glPopMatrix();
+                Gl.glFlush();
+                AnT.Invalidate();
+
+               
+            }
+            else if (radioButton3.Checked)//texture
+            {
+
+            }
+                //Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
+                //Gl.glLoadIdentity();
+                //Gl.glColor3f(Color.R, Color.G, Color.B);
+                //// масштаб
+                //Gl.glScalef(x_scale, y_scale, z_scale);
+                //Gl.glPushMatrix();
+                //Gl.glTranslated(0, 0, -6);
+                //// перемещаем камеру для более хорошего обзора объекта
+                //Gl.glTranslated(x_tr, y_tr, z_tr);
+                ////// включаем режим текстурирования
+                ////Gl.glEnable(Gl.GL_TEXTURE_2D);
+
+                ////// включаем режим текстурирования, указывая идентификатор mGlTextureObject
+                ////Gl.glBindTexture(Gl.GL_TEXTURE_2D, mGlTextureObject);
+
+                //Gl.glRotated(angle, 1, 1, 0);
+                //// рисуем сферу с помощью библиотеки FreeGLUT
+                ////Glut.glutWireSphere(0.7, 30, 30);
+                //Glut.glutSolidSphere(0.7, 30, 30);
+                ////Glut.glutWireCylinder(1,2,50,1);
+                
+
+                //// Модель освещенности с одним источником цвета
+                //float[] light_position = { 10, 10, -30, 0 }; // Координаты источника света
+                //float[] lghtClr = { 1, 1, 1, 0 }; // Источник излучает белый цвет
+                //float[] mtClr = { 0, 1, 0, 0 }; // Материал зеленого цвета
+                //Gl.glPolygonMode(Gl.GL_FRONT, Gl.GL_FILL); // Заливка полигонов
+                //Gl.glShadeModel(Gl.GL_SMOOTH); // Вывод с интерполяцией цветов
+                //Gl.glEnable(Gl.GL_LIGHTING); // Будем рассчитывать освещенность
+                //Gl.glLightfv(Gl.GL_LIGHT0, Gl.GL_POSITION, light_position);
+                //Gl.glLightfv(Gl.GL_LIGHT0, Gl.GL_AMBIENT, lghtClr); // Рассеивание
+                //Gl.glEnable(Gl.GL_LIGHT0); // Включаем в уравнение освещенности источник GL_LIGHT0
+                //// Диффузионная компонента цвета материала
+                //Gl.glMaterialfv(Gl.GL_FRONT, Gl.GL_DIFFUSE, mtClr);
+                //// Выводим тонированный glut-примитив
+                //showSolid();
+                //Gl.glDisable(Gl.GL_LIGHTING); // Будем рассчитывать освещенность
+
+                //Gl.glPopMatrix();
+                //Gl.glFlush();
+                //AnT.Invalidate();
+            
+        }
+
+        /// <summary>
+        /// Поворот и перемещение
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AnT_KeyDown(object sender, KeyEventArgs e)
+        {
+            //Поворот Право-Х Лево-Z
+            if (e.KeyCode == Keys.X)
+            {
+                angle += 5;
+            }
+            if (e.KeyCode == Keys.Z)
+            {
+                angle -= 5;
+            }
+            // W A S D отвечают за перенос
+            if (e.KeyCode == Keys.D) //вправо
+            {
+                x_tr += 0.05f;
+            }
+            else if (e.KeyCode == Keys.W)//вверх
+            {
+                y_tr += 0.05f;
+            }
+            else if (e.KeyCode == Keys.S)//вниз
+            {
+                y_tr -= 0.05f;
+            }
+            else if (e.KeyCode == Keys.A)//влево
+            {
+                x_tr -= 0.05f;
+            }
+            else if (e.KeyCode == Keys.Q)//Масштаб -
+            {
+                if (comboBox1.SelectedIndex == 0)//x
+                {
+                    x_scale /= 1.05f;
+                }
+                else if (comboBox1.SelectedIndex == 1)//y
+                {
+                    y_scale /= 1.05f;
+                }
+                else if (comboBox1.SelectedIndex == 1)//z
+                {
+                    z_scale /= 1.05f;
+                }
+            }
+            else if (e.KeyCode == Keys.E)//Масштаб +
+            {
+                if (comboBox1.SelectedIndex == 0)//x
+                {
+                    x_scale *= 1.05f;
+                }
+                else if (comboBox1.SelectedIndex == 1)//y
+                {
+                    y_scale *= 1.05f;
+                }
+                else if (comboBox1.SelectedIndex == 1)//z
+                {
+                    z_scale *= 1.05f;
+                }
+            }
+        }
+
+        private void RenderTimer_Tick(object sender, EventArgs e)
+        {
+            Draw();
+        }
+
+        /// <summary>
+        /// Выбор оси в combobox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            AnT.Focus();
+        }
+
+        /// <summary>
+        /// Цвет
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Color color = colorDialog1.Color;
+                Color = color;
+                pictureBox1.BackColor = color;
+            }
+        }
+
+        // Создание текстуры в памяти openGL
+        private static uint MakeGlTexture(int Format, IntPtr pixels, int w, int h)
+        {
+            // идентификатор текстурного объекта
+            uint texObject;
+            // генерируем текстурный объект
+            Gl.glGenTextures(1, out texObject);
+            // устанавливаем режим упаковки пикселей
+            Gl.glPixelStorei(Gl.GL_UNPACK_ALIGNMENT, 1);
+            // создаем привязку к только что созданной текстуре
+            Gl.glBindTexture(Gl.GL_TEXTURE_2D, texObject);
+            // устанавливаем режим фильтрации и повторения текстуры
+            Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_WRAP_S, Gl.GL_REPEAT);
+            Gl.glTexParameteri(Gl.GL_TEXTURE_3D, Gl.GL_TEXTURE_WRAP_T, Gl.GL_REPEAT);
+
+            Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_MAG_FILTER, Gl.GL_NEAREST);
+            Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_MIN_FILTER, Gl.GL_NEAREST);
+            Gl.glTexEnvf(Gl.GL_TEXTURE_ENV, Gl.GL_TEXTURE_ENV_MODE, Gl.GL_REPLACE);
+
+            // создаем RGB или RGBA текстуру
+            switch (Format)
+            {
+                case Gl.GL_RGB:
+                    Gl.glTexImage2D(Gl.GL_TEXTURE_2D, 0, Gl.GL_RGB, w, h, 0, Gl.GL_RGB, Gl.GL_UNSIGNED_BYTE, pixels);
+                    break;
+                case Gl.GL_RGBA:
+                    Gl.glTexImage2D(Gl.GL_TEXTURE_2D, 0, Gl.GL_RGBA, w, h, 0, Gl.GL_RGBA, Gl.GL_UNSIGNED_BYTE, pixels);
+                    break;
+            }
+            // возвращаем идентификатор текстурного объекта
+            return texObject;
+        }
+
+        /// <summary>
+        /// Загрузка текстуры
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // открываем окно выбора файла
+            DialogResult res = openFileDialog1.ShowDialog();
+            // если файл выбран - и возвращен результат O
+            if (res == DialogResult.OK)
+            {
+                // создаем изображение с идентификатором imageId
+                Il.ilGenImages(1, out imageId);
+                // делаем изображение текущим
+                Il.ilBindImage(imageId);
+                // адрес изображения полученный с помощью окна выбора файла
+                string url = openFileDialog1.FileName;
+                // пробуем загрузить изображение
+                if (Il.ilLoadImage(url))
+                {
+                    // если загрузка прошла успешно 
+                    // сохраняем размеры изображения
+                    int width = Il.ilGetInteger(Il.IL_IMAGE_WIDTH);
+                    int height = Il.ilGetInteger(Il.IL_IMAGE_HEIGHT);
+                    // определяем число бит на пиксель
+                    int bitspp = Il.ilGetInteger(Il.IL_IMAGE_BITS_PER_PIXEL);
+                    switch (bitspp)
+                        // в зависимости от полученного результата
+                    {
+                        // создаем текстуру, используя режим GL_RGB или GL_RGBA
+                        case 24:
+                            mGlTextureObject = MakeGlTexture(Gl.GL_RGB, Il.ilGetData(), width, height);
+                            break;
+                        case 32:
+                            mGlTextureObject = MakeGlTexture(Gl.GL_RGBA, Il.ilGetData(), width, height);
+                            break;
+                    }
+                    // активируем флаг, сигнализирующий загрузку текстуры
+                    textureIsLoad = true;
+                    // очищаем память
+                    Il.ilDeleteImages(1, ref imageId);
+                }
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Обновить
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RenderTimer.Stop();
             Glut.glutInit();
             Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH);
             Gl.glClearColor(255, 255, 255, 1);
@@ -41,45 +385,11 @@ namespace Lb6Paint
             Gl.glLoadIdentity();
             // настройка параметров OpenGL для визуализации
             Gl.glEnable(Gl.GL_DEPTH_TEST);
-
-            //
-            Draw();
-        //
-        string path = "C:\\Users\\aken\\Desktop\\cosmos";
-
-        }
-
-        private void Draw()
-        {
-            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
-            Gl.glLoadIdentity();
-            Gl.glColor3f(0, 1, 1);
-            Gl.glPushMatrix();
-            Gl.glTranslated(0, 0, -6);
-            Gl.glRotated(angle, 1, 1, 0);
-            // рисуем сферу с помощью библиотеки FreeGLUT
-            Glut.glutWireSphere(0.7, 50, 50);
-            Glut.glutWireCylinder(1,2,50,1);
-            Gl.glPopMatrix();
-            Gl.glFlush();
-            AnT.Invalidate();
-        }
-        private void AnT_KeyDown(object sender, KeyEventArgs e)
-        {
-            //Поворот Право-Х Лево-Z
-            if (e.KeyCode == Keys.X)
-            {
-                angle += 5;
-            }
-            if (e.KeyCode == Keys.Z)
-            {
-                angle -= 5;
-            }
-        }
-
-        private void RenderTimer_Tick(object sender, EventArgs e)
-        {
-            Draw();
+            // начало визуализации (активируем таймер)
+            RenderTimer.Start();
+            // инициализация библиотеки openIL
+            Il.ilInit();
+            Il.ilEnable(Il.IL_ORIGIN_SET);
         }
     }
 }
